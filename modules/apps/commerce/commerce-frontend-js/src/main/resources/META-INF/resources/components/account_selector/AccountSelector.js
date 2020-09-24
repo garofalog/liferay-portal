54605eca1124f433290a7e174a03087f533c458a
Giulio Garofalo <giulio.garofalo@liferay.com>
2020-09-24 11:21:49 +0200

    COMMERCE-4617  fabio's changes

--- modules/apps/commerce/commerce-frontend-js/src/main/resources/META-INF/resources/components/account_selector/AccountSelector.js
+++ modules/apps/commerce/commerce-frontend-js/src/main/resources/META-INF/resources/components/account_selector/AccountSelector.js
@@ -32,12 +32,27 @@ import ClayDropDown from '@clayui/drop-down';
 import ClayIcon from '@clayui/icon';
 
 // import ClayIcon, {ClayIconSpriteContext} from '@clayui/icon';
+
 import PropTypes from 'prop-types';
 import React, { useCallback, useContext, useEffect, useRef, useState } from 'react';
 import { CSSTransition } from 'react-transition-group';
 
 import { getData, getValueFromItem } from '../../utilities/index';
 import Autocomplete from './../autocomplete/Autocomplete'
+
+function AccountsTable(props) {
+	return props.items && (
+		<ul>
+			{props.items.map(item => {
+				return (
+					<li key={item.id} onClick={(e) => console.log(e)}>
+						{item.name}
+					</li>
+				)
+			})}
+		</ul>
+	)
+}
 
 function AccountSelector(props) {
 	const [active, setActive] = useState(false);
@@ -48,43 +63,10 @@ function AccountSelector(props) {
 	const [menuHeight, setMenuHeight] = useState("200");
 	const dropdownRef = useRef(null);
 
-	// function _getAccounts() {
-	// 	return getData(props.accountsAPI + 'search-accounts').then(data => {
-	// 		setAccounts(data.accounts);
-	// 	});
-	// }
-
-	const getOrders = (id) => {
-		getData(props.accountsAPI + 'search-accounts/' + id + "/orders").then(data => {
-			setOrders(data.accounts);
-		});
-	}
-
-	useEffect(() => {
-		if (active && !accounts) {
-			getData(props.accountsAPI + 'search-accounts/').then(data => {
-				setAccounts(data.accounts);
-			});
-		}
-	}, [accounts, active, props.accountsAPI])
-
 	useEffect(() => {
 		const dropRef = dropdownRef.current ? dropdownRef.current.firstChild.offsetHeight : null
 		setMenuHeight(dropRef)
 	}, [])
-
-	function DropdownItem(props) {
-		return (
-			<a className="menu-item"
-				href="#"
-				onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}>
-				<span className="icon-button">{props.leftIcon}</span>
-				{props.children}
-				<span className="icon-right">{props.rightIcon}</span>
-			</a>
-
-		);
-	}
 
 	function calcHeight(el) {
 		const height = el.offsetHeight;
@@ -92,8 +74,6 @@ function AccountSelector(props) {
 	}
 
 	return (
-
-
 		<ClayDropDown
 			active={active}
 			onActiveChange={setActive}
@@ -106,7 +86,6 @@ function AccountSelector(props) {
 						src="http://lorempixel.com/75/50/abstract/"
 						width="40"
 					/>
-					{/* <span className="b-dropdown-initials my-auto p-2">FS</span> */}
 					<div className="account-selector-info ml-3">
 						<h6>Forward Auto Service</h6>
 						<div className="account-selector-info-details d-flex">
@@ -114,143 +93,54 @@ function AccountSelector(props) {
 						</div>
 					</div>
 				</button>
-
 			}
 		>
+			<CSSTransition
+				className="menu-primary"
+				in={activeMenu === 'main'}
+				onEnter={calcHeight}
+				timeout={500}
+				unmountOnExit
+			>
+				<h2>{Liferay.Language.get('accounts')}</h2>
+				<Autocomplete
+					alwaysActive={true}
+					apiUrl="/o/headless-commerce-admin-account/v1.0/accounts/"
+					autofill={true}
+					customView={AccountsTable}
+					infinityScrollMode={true}
+					inputName="account-search"
+					itemsKey="id"
+					itemsLabel="name"
+					onItemSelected={
+						console.log
+					}
+				/>
+			</CSSTransition>
 
-			<ClayDropDown.ItemList>
-
-				<CSSTransition
-					className="menu-primary"
-					in={activeMenu === 'main'}
-					onEnter={calcHeight}
-					timeout={500}
-					unmountOnExit>
-
-					<div className="menu">
-
-						<h2>Accounts</h2>
-
-						<Autocomplete
-							apiUrl="/account-selector/search-accounts"
-							autofill={true}
-							id="cul"
-							infinityScrollMode={true}
-							inputName="account-search"
-							itemsKey="accountId"
-							itemsLabel="name"
-							onItemSelected={item => 
-							// 	{
-							// 	<DropdownItem
-							// 		goToMenu="settings"
-							// 		leftIcon={"CogIcon"}
-							// 		rightIcon={"<ChevronIcon />"}>
-							// 		{item.accountId}
-							// 	</DropdownItem>
-							// 	}
-							// }
-
-								{
-								console.log(item)
-
-								// goToMenu("secondary")
-
-								getOrders(item.accountId)
-							}}
-
-						/>
-
-						{accounts && accounts.length === 0 && (
-							<ClayDropDown.Group header="Group #1">
-								<ClayDropDown.Item>
-									No Accounts Found
-										</ClayDropDown.Item>
-							</ClayDropDown.Group>
-						)}
-
-						<DropdownItem
-							goToMenu="settings"
-							leftIcon={"CogIcon"}
-							rightIcon={"<ChevronIcon />"}>
-							Settings
-						</DropdownItem>
-						<DropdownItem
-							goToMenu="animals"
-							leftIcon="🦧"
-							rightIcon={"<ChevronIcon />"}>
-							Animals
-						</DropdownItem>
-
-					</div>
-				</CSSTransition>
-
-				{/* orders.orders[0].accountId */}
-				<CSSTransition
-					classNames="menu-secondary"
-					in={activeMenu === "account1"}
-					key="account1"
-					onEnter={calcHeight}
-					timeout={100}
-					unmountOnExit>
-
-					<Autocomplete
-						apiUrl="/account-selector/search-accounts"
-						autofill={true}
-						id="yoyo"
-						infinityScrollMode={true}
-						inputName="account-search"
-						itemsKey="accountId"
-						itemsLabel="name"
-						onItemSelected={item => {
-							console.log(item)
-
-						}}
-
-					/>
-
-				</CSSTransition>
-
-				<CSSTransition
-					classNames="menu-secondary"
-					in={activeMenu === 'settings'}
-					onEnter={calcHeight}
-					timeout={500}
-					unmountOnExit>
-					<div className="menu">
-						<DropdownItem
-							goToMenu="main"
-							leftIcon={
-								<ClayIcon spritemap={props.spritemap} symbol="order-arrow-left" />
-							}
-						>
-							<h2>My Tutorial</h2>
-						</DropdownItem>
-						<DropdownItem leftIcon={"<BoltIcon />"}>HTML</DropdownItem>
-						<DropdownItem leftIcon={"<BoltIcon />"}>CSS</DropdownItem>
-						<DropdownItem leftIcon={"<BoltIcon />"}>JavaScript</DropdownItem>
-						<DropdownItem leftIcon={"<BoltIcon />"}>Awesome!</DropdownItem>
-					</div>
-				</CSSTransition>
-
-				<CSSTransition
-					classNames="menu-secondary"
-					in={activeMenu === 'animals'}
-					onEnter={calcHeight}
-					timeout={500}
-					unmountOnExit>
-					<div className="menu">
-						<DropdownItem goToMenu="main" leftIcon={"<ArrowIcon />"}>
-							<h2>Animals</h2>
-						</DropdownItem>
-						<DropdownItem leftIcon="🦘">Kangaroo</DropdownItem>
-						<DropdownItem leftIcon="🐸">Frog</DropdownItem>
-						<DropdownItem leftIcon="🦋">Horse?</DropdownItem>
-						<DropdownItem leftIcon="🦔">Hedgehog</DropdownItem>
-					</div>
-				</CSSTransition>
-
-			</ClayDropDown.ItemList>
-
+			<CSSTransition
+				classNames="menu-secondary"
+				in={activeMenu === "account1"}
+				key="account1"
+				onEnter={calcHeight}
+				timeout={100}
+				unmountOnExit
+			>
+			</CSSTransition>
+				<h2>{Liferay.Language.get('orders')}</h2>
+				<Autocomplete
+					alwaysActive={true}
+					apiUrl="/o/headless-commerce-admin-account/v1.0/accounts/"
+					autofill={true}
+					customView={AccountsTable}
+					infinityScrollMode={true}
+					inputName="account-search"
+					itemsKey="id"
+					itemsLabel="name"
+					onItemSelected={
+						console.log
+					}
+				/>
 		</ClayDropDown>
 	);
 }
