91709090b9c0283883baa778f11146e7d279fc42
Giulio Garofalo <giulio.garofalo@liferay.com>
2020-10-07 13:22:19 +0200

    first two tests done

--- modules/apps/commerce/commerce-frontend-js/test/account_selector/account_selector_test.es.js
+++ modules/apps/commerce/commerce-frontend-js/test/account_selector/account_selector_test.es.js
@@ -13,45 +13,57 @@
  */
 
 import '@testing-library/jest-dom/extend-expect';
-import {cleanup, fireEvent, render, screen} from '@testing-library/react';
+import {cleanup, fireEvent, render, waitForElement} from '@testing-library/react';
 import fetchMock from 'fetch-mock';
 import React from 'react';
-
+import ReactDOM from 'react-dom';
+import { accountsList, ordersList } from '../test-utilities/headless-commerce-accounts'
 import AccountSelector from '../../src/main/resources/META-INF/resources/components/account_selector/AccountSelector';
 
+const WAIT_MS = 200
+
 describe('AccountSelector', () => {
-    let accounts
-    let orders
+
+    let renderedComponent
+
+    afterEach(() => { 
+        cleanup()
+        fetchMock.restore();
+    })
 
     
-	afterEach(cleanup);
+    beforeEach(() => {
+        fetchMock.mock(/o\/headless-commerce-admin-account\/v1.0\/accounts/, accountsList)
 
-    beforeAll(() => {
-        // fetchMock.mock("account-mock", () => {
-        //     accounts = 
-        // })
-    })
-	beforeEach(() => {});
-
-	it('render "select-account-and-order" the accountselect', () => {
-        const component = render(
-			<AccountSelector
-				createNewOrderUrl="/asdasdasd"
-				id="Test"
-				spritemap="./assets/icons.svg"
-				viewOrderUrl="/test-url/{id}"
-			/>
+        renderedComponent = render(
+            <AccountSelector
+                apiUrl="/o/headless-commerce-admin-account/v1.0/accounts/"
+                createNewOrderURL="/asdasdasd"
+                id="Test"
+                spritemap="./assets/icons.svg"
+                viewOrderUrl="/test-url/{id}"
+                setCurrentAccountURL="/account-selector/setCurrentAccounts"
+                selectOrderURL="/test-url/{id}"
+            />
         );
-        const { container, getByText } = render(component)
-        expect(component.getByText('select-account-and-order')).toBeInTheDocument()
+        // fireEvent.click(renderedComponent.getByText("select-account-and-order"))
+    })
 
-	});
+
+	it('display "select-account-and-order" the accountselect', () => {
+        expect(renderedComponent.getByText('select-account-and-order')).toBeInTheDocument()
+    });
+
+    it('display "select-accounts"', async () => {
+
+        fireEvent.click(renderedComponent.getByText("select-account-and-order"));
+        const resolvedValue = await waitForElement(() => renderedComponent.getByText('select-accounts'))
+        expect(resolvedValue).toBeInTheDocument()
+        let li = renderedComponent.getAllByRole('listitem')
+        expect(resolvedValue).toHaveClass('dropdown-subheader')
+        expect(li.length).toBe(10)
+        console.log(screen)
+        // console.log(Object.getOwnPropertyNames(screen));
+    })
+
 });
-
-
-
-// expect(screen.getByRole('button')).toHaveTextContent(
-// 	'select-account-and-order'
-// );
-
-// fireEvent.load(screen.load())
