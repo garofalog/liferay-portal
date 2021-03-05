959cc9f70a85ec129fa92ffc68355651e0a0ee09
garofalog <giulio.garofalo@liferay.com>
2021-03-05 11:22:26 +0100

    create circle on click

--- modules/apps/commerce/commerce-frontend-js/src/main/resources/META-INF/resources/components/d3diagram/Circles.js
+++ modules/apps/commerce/commerce-frontend-js/src/main/resources/META-INF/resources/components/d3diagram/Circles.js
@@ -15,7 +15,7 @@
 // import * as d3 from "d3";
 // console.log(d3)
 
-import { drag, event, mouse, select, transform, zoom } from 'd3';
+import { drag, event, mouse, select, zoom } from 'd3';
 import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
 
 import debounce from '../../utilities/debounce';
@@ -54,11 +54,7 @@ const Circles = ({ data }) => {
     // const [zoomableLayer, setZoomableLayer] = useState()
     // const [zoom, setZoom] = useState()
 
-    let container;
-
-    // const nodes
-
-    // let zoomableLayer;
+    let container
 
 
     useEffect(() => {
@@ -79,21 +75,6 @@ const Circles = ({ data }) => {
 
     }, [data.length]);
     
-    
-
-    ////////////////////////////////////////////////////////
-
-    // const dragstarted = () => {
-    //     event.sourceEvent.stopPropagation();
-    // }
-
-    // const dragged = (d) => {
-    //     d.x = event.x;
-    //     d.y = event.y;
-    //     select(this).attr({
-    //         transform: "translate(" + d.x + "," + d.y + ")"
-    //     });
-    // }
 
     
     function dragged() {
@@ -104,123 +85,51 @@ const Circles = ({ data }) => {
         console.log(`${event.x}, ${event.y}`);
     }
 
-    // const drag = drag()
-    //     .origin((d) => d)
-    //     .on("dragstart", dragstarted)
-    //     .on("drag", dragged)
+    // const handleDrag = drag()
+    //     .subject(function () {
+    //         const me = select(this);
 
-
-    const handleDrag = drag()
-        .subject(function () {
-            const me = select(this);
-
-            return { x: me.attr('cx'), y: me.attr('cy') }
-        })
-        .on('drag', () => {
-            const me = select(this);
-            me.attr('cx', event.x);
-            me.attr('cy', event.y);
-        });
-    
-    
-    // const node = ReactDOM.findDOMNode(this);
-    // }
-
-    // example 2
-
-    // var dragHandler = drag().on("drag", () => {
-    //         select(this)
-    //             .attr("x", event.x)
-    //             .attr("y", event.y);
+    //         return { x: me.attr('cx'), y: me.attr('cy') }
+    //     })
+    //     .on('drag', () => {
+    //         const me = select(this);
+    //         me.attr('cx', event.x);
+    //         me.attr('cy', event.y);
     //     });
-
+    
     var dragHandler = drag()
         .on('drag', dragged);
     
 
     useLayoutEffect(() => {
 
+        
+
         var zoomEV = zoom()
             .on("zoom", () => {
                 container.attr("transform", event.transform);
             });
 
-        container = select('g').call(zoomEV)
-
-
-
-        container.on("click", function () {
-            const mouseEv = mouse(this);
-
-            var pointer = container
-                .append("use")
-                .datum({ x: mouseEv[0], y: mouseEv[1], selected: false })
-                .attr("id", "currentPointer")
-                .attr("href", "#pointer")
-                .attr("transform", "translate(" + mouseEv[0] + "," + mouseEv[1] + ")scale(0)")
-                .attr("fill", "#039BE5")
-                .attr("stroke", "#039BE5")
-                .attr("stroke-width", "1px");
-
-            pointer
-                .transition()
-                .duration(500)
-                .attr("transform", "translate(" + mouseEv[0] + "," + mouseEv[1] + ") scale(1)")
-                .attr("x", mouseEv[0])
-                .attr("y", mouseEv[1])
-                .attr("transform", "scale(1)");
-
-            pointer.on("click", () => {
-                if (event.ctrlKey) {
-                    pointer.transition()
-                        .duration(500)
-                        .attr("transform", "translate(" + pointer.attr("x") + "," + pointer.attr("y") + ") scale(0)")
-                        .remove();
-                } else {
-                    
-                    var datum = pointer.datum();
-                    if (pointer.datum().selected) {
-                        datum.selected = false;
-                        pointer
-                            .datum(datum)
-                            .transition()
-                            .duration(500)
-                            .attr("stroke", "#039BE5")
-                            .attr("stroke-width", "1px");
-                    } else {
-                        datum.selected = true;
-                        pointer
-                            .datum(datum)
-                            .transition()
-                            .duration(500)
-                            .attr("stroke", "#455A64")
-                            .attr("stroke-width", "3px");
-                    }
-
-                }
-                event.stopPropagation();
-            });
-
-            var dragHandler = drag().on("drag", function (d) {
-                    console.log('in dragHandler')
-                    select(this)
-                        .attr("x", d.x = event.x)
-                        .attr("y", d.y = event.y);
-                });
-            
-
-            dragHandler(pointer);
-        });
-
-
-
+        container = select('g')
+ 
         container.append("image")
             .attr("xlink:href", "https://www.zooplus.it/magazine/wp-content/uploads/2020/10/1-37-768x512.jpg")
             .attr("width", 960)
             .attr("height", 450)
-            
+
+        container.call(zoomEV)
+
+        const circ = container.append("circle")
+            .attr("cx", 50)
+            .attr("cy", 50)
+            .attr("r", 50)
+            .style("fill", "#ff0")
+            .classed('draggable', true)
+
         if (Array.isArray(data)) {
-        
+
+
+            
 
             select('g').append("circle")
                 .attr("cx", 50)
@@ -236,52 +145,92 @@ const Circles = ({ data }) => {
                 .style("fill", "#ae2")
                 .classed('draggable', true)
 
-            select('g').append("rect")
-                .attr("x", 50)
-                .attr("y", 50)
-                .attr("width", 100)
-                .attr("height", 50)
-                .attr("stroke", "#C1272D")
-                .attr("stroke-width", 5)
-                .style("fill", "#F7931E")
-                .classed('draggable', true) 
-
-            // select('g').selectAll('circle').data(data)
-            //     .enter()
-            //     .append('circle')
-            //     .merge(update)
-            //     .attr('r', (d) => d)
-            //     .attr('cx', (_, i) => width * (i + 1))
-            //     .attr('cy', () => Math.random() * 100)
-            //     .attr('stroke', (_, i) => (i % 2 === 0 ? '#f80' : '#aaf'))
-            //     .attr('fill', (_, i) => (i % 2 === 0 ? 'orange' : '#44f'))
-
-            // update.exit().remove()
-
-            // const update = select('#second').selectAll('circle').data(data);
-            // update
-            //     .enter()
-            //     .append('circle')
-            //     .attr('r', (d) => d)
-            //     .attr('cx', (_, i) => width * (i + 1))
-            //     .attr('cy', () => Math.random() * 100)
-            //     .attr('stroke', (_, i) => (i % 2 === 0 ? '#f80' : '#aaf'))
-            //     .attr('fill', (_, i) => (i % 2 === 0 ? 'orange' : '#44f'));
-
-            // update
-            //     .attr('r', (d) => d)
-            //     .attr('cx', (_, i) => width * (i + 1))
-            //     .attr('cy', () => Math.random() * 100)
-            //     .attr('stroke', (_, i) => (i % 2 === 0 ? '#f80' : '#aaf'))
-            //     .attr('fill', (_, i) => (i % 2 === 0 ? 'orange' : '#44f'));
-
-            // update.exit().remove();
-
-            
-
         }
 
-        // nodes = container.selectAll(".draggable").call(drag).data(nodesData);
+        container.on("click", function () {
+            console.log('in click')
+
+            const mouseEv = mouse(this);
+            
+            var pointer = container.append("circle")
+
+                // .datum({
+                //     selected: false,
+                //     x: mouseEv[0], 
+                //     y: mouseEv[1] 
+                // })
+            //     .attr("transform", "translate(" + mouseEv[0] + "," + mouseEv[1] + ")scale(0)")
+
+                .attr("cx", mouseEv[0] || 50)
+                .attr("cy", mouseEv[1] || 50)
+                .attr("r", 50)
+                .style("fill", "#ff0")
+                .classed('draggable', true)
+
+            // container
+            //     .append("use")
+            //     .datum({ x: mouseEv[0], y: mouseEv[1], selected: false })
+            //     .attr("id", "currentPointer")
+            //     .attr("href", "#pointer")
+            //     .attr("transform", "translate(" + mouseEv[0] + "," + mouseEv[1] + ")scale(0)")
+            //     .attr("fill", "#039BE5")
+            //     .attr("stroke", "#039BE5")
+            //     .attr("stroke-width", "1px");
+
+            circ
+                .transition()
+                .duration(500)
+
+                .attr("transform", "translate(" + mouseEv[0] + "," + mouseEv[1] + ") scale(1)")
+
+                .attr("x", mouseEv[0])
+                .attr("y", mouseEv[1])
+                .attr("transform", "scale(1)");
+
+            circ.on("click", () => {
+
+                if (event.ctrlKey) {
+                    circ.transition()
+                        .duration(500)
+                        .attr("transform", "translate(" + circ.attr("x") + "," + circ.attr("y") + ") scale(0)")
+                        .remove();
+                } else {
+                    
+                    var datum = circ.datum();
+                    if (circ.datum().selected) {
+                        datum.selected = false;
+                        circ
+                            .datum(datum)
+                            .transition()
+                            .duration(500)
+                            .attr("stroke", "#039BE5")
+                            .attr("stroke-width", "1px");
+                    } else {
+                        datum.selected = true;
+                        circ
+                            .datum(datum)
+                            .transition()
+                            .duration(500)
+                            .attr("stroke", "#455A64")
+                            .attr("stroke-width", "3px");
+                    }
+
+                }
+                event.stopPropagation();
+            });
+
+            var dragHandler2 = drag().on("drag", function (d) {
+                    console.log('in dragHandler2')
+                    select(this)
+                        .attr("x", d.x = event.x)
+                        .attr("y", d.y = event.y);
+                });
+            
+
+            dragHandler2(circ);
+            dragHandler(container.selectAll('.draggable'));
+
+        });
 
         dragHandler(container.selectAll('.draggable'));
         
