45a2ead5c7eb7b7153c99935af04ff045d12ef5b
garofalog <giulio.garofalo@liferay.com>
2021-04-21 19:16:35 +0200

    COMMERCE-5483 added files

---
+++ modules/apps/commerce/commerce-frontend-js/src/main/resources/META-INF/resources/components/diagram/NavigationsUtils.js
@@ -0,0 +1,67 @@
+export const moveRight = (container, navigationController) => {
+    const position = container.attr('transform');
+    const vai = position.match(/(-?[0-9]+[.,-\s]*)+/g);
+    const co = vai[0].split(',').map((x) => parseInt(x, 10));
+    const s = {
+        k: parseFloat(vai[1]),
+        x: co[0] + navigationController.dragStep,
+        y: co[1],
+    };
+    // setImageState(s);
+    container.attr(
+        'transform',
+        `translate(${s.x},${s.y}) scale(${s.k})`
+    );
+};
+export const moveLeft = (container, navigationController) => {
+    const position = container.attr('transform');
+    const vai = position.match(/(-?[0-9]+[.,-\s]*)+/g);
+    const co = vai[0].split(',').map((x) => parseInt(x, 10));
+    const s = {
+        k: parseFloat(vai[1]),
+        x: co[0] - navigationController.dragStep,
+        y: co[1],
+    };
+    // setImageState(s);
+    container.attr(
+        'transform',
+        `translate(${s.x},${s.y}) scale(${s.k})`
+    );
+};
+export const moveUp = (container, navigationController) => {
+    const position = container.attr('transform');
+    const vai = position.match(/(-?[0-9]+[.,-\s]*)+/g);
+    const co = vai[0].split(',').map((x) => parseInt(x, 10));
+    const s = {
+        k: parseFloat(vai[1]),
+        x: co[0],
+        y: co[1] - navigationController.dragStep,
+    };
+    // setImageState(s);
+    container.attr(
+        'transform',
+        `translate(${s.x},${s.y}) scale(${s.k})`
+    );
+};
+export const moveDown = (container, navigationController) => {
+    const position = container.attr('transform');
+    const vai = position.match(/(-?[0-9]+[.,-\s]*)+/g);
+    const co = vai[0].split(',').map((x) => parseInt(x, 10));
+    const s = {
+        k: parseFloat(vai[1]),
+        x: co[0],
+        y: co[1] + navigationController.dragStep,
+    };
+    // setImageState(s);
+    container.attr(
+        'transform',
+        `translate(${s.x},${s.y}) scale(${s.k})`
+    );
+};
+
+export const zoomIn = (container, panZoom) => {
+    panZoom.scaleBy(container.transition().duration(400), 1.2);
+};
+export const zoomOut = (container, panZoom) => {
+    panZoom.scaleBy(container.transition().duration(400), 0.8);
+};
---
+++ modules/apps/commerce/commerce-frontend-js/src/main/resources/META-INF/resources/components/diagram/PinsUtils.js
@@ -0,0 +1,17 @@
+
+
+// export const addPin = () => {
+//     setCpins(
+//         cPins.concat({
+//             cx: 50,
+//             cy: 50,
+//             draggable: true,
+//             fill: '#' + addNewPinState.fill,
+//             id: cPins.length,
+//             linked_to_sku: 'sku',
+//             quantity: 0,
+//             r: addNewPinState.radius,
+//             sku: addNewPinState.sku,
+//         })
+//     );
+// };
