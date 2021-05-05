5c6072c87a3e73fd389b86e27e20a45993374e71
garofalog <giulio.garofalo@liferay.com>
2021-05-05 16:22:36 +0200

    COMMERCE-5483 tests part1/2

---
+++ modules/apps/commerce/commerce-frontend-js/test/components/Diagram/Diagram.js
@@ -0,0 +1,111 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import { act, cleanup, fireEvent, getByAltText, render, wait } from '@testing-library/react';
+import { event, select, zoom, zoomIdentity, zoomTransform } from 'd3';
+import React from 'react';
+
+import Diagram from '../../../src/main/resources/META-INF/resources/components/Diagram/Diagram';
+
+describe('Diagram 1 of 5 features', () => {
+    const initialProps = {
+        image: 'https://i0.wp.com/detoxicrecenze.com/wp-content/uploads/2018/05/straight-6-engine-diagram-460-ford-engine-diagram-wiring-info-e280a2-of-straight-6-engine-diagram.jpg'
+    }
+
+    beforeEach(() => {
+        jest.resetAllMocks();
+
+        const onUpdateSpy = jest.fn();
+    });
+
+    afterEach(() => {
+        cleanup();
+    });
+
+    it('Diagram image loads', () => {
+        const { container } = render(<Diagram {...initialProps} />);
+        const image = container.querySelector('image')
+        expect(image.href).toBe(initialProps.href)
+    });
+
+    it('If Zoom Controller is enabled, will render the component', () => {
+        const { container } = render(<Diagram {...initialProps} zoomController={{
+            enable: true,
+            position: {
+                bottom: '0px',
+                left: '',
+                right: '200px',
+                top: '',
+            }}} />);
+
+        const controller = container.querySelector('#zoom-controller')
+
+        expect(controller).toBeTruthy();
+    });
+
+
+    it('If Zoom Controller is disabled, it won\'t be rendered', () => {
+        const { container } = render(<Diagram {...initialProps} zoomController={{ enable: false}} />);
+
+        const controller = container.querySelector('#zoom-controller')
+        
+        expect(controller).toBeFalsy();
+    });
+
+    it('If Navigation Controller is enabled, will render the component', () => {
+        const { container } = render(<Diagram {...initialProps} navigationController={{
+            dragStep: 10,
+            enable: true,
+            enableDrag: false,
+            position: {
+                bottom: '15px',
+                left: '',
+                right: '50px',
+                top: '',
+            },
+        }} />);
+
+        const controller = container.querySelector('#move-controller')
+
+        expect(controller).toBeTruthy();
+    });
+
+    it('If Navigation Controller is disabled, it won\'t be rendered', () => {
+        const { container } = render(<Diagram {...initialProps} navigationController={{enable: false}} />);
+
+        const controller = container.querySelector('#move-controller')
+
+        expect(controller).toBeFalsy();
+    });
+
+    // it('Diagram image moves right if <Move-Right-Button> is clicked', () => {
+        
+    // });
+
+    // it('Diagram image zooms in if <Zoom-In-Button> is clicked', () => {
+
+    // });
+
+    // it('Diagram image moves if enableDrag property is enabled', () => {
+
+    // });
+
+    // it('Diagram zooms changing value on zoom <select>', () => {
+
+    // });
+
+    it('On <Reset-Button> click, the diagram image scale to 1', () => {
+
+    });
+});
