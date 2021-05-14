cd0de82d86c433c52628e6e05b67316ccc4e8317
garofalog <giulio.garofalo@liferay.com>
2021-05-14 16:57:29 +0200

    COMMERCE-5483 initial commit

--- modules/dxp/apps/commerce/commerce-shop-by-diagram/commerce-shop-by-diagram-web/webpack.config.dev.js
+++ modules/dxp/apps/commerce/commerce-shop-by-diagram/commerce-shop-by-diagram-web/webpack.config.dev.js
@@ -1,12 +1,15 @@
 /**
  * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
  *
- * The contents of this file are subject to the terms of the Liferay Enterprise
- * Subscription License ("License"). You may not use this file except in
- * compliance with the License. You can obtain a copy of the License by
- * contacting Liferay, Inc. See the License for the specific language governing
- * permissions and limitations under the License, including but not limited to
- * distribution rights of the Software.
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
  */
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -15,6 +18,11 @@ const webpack = require('webpack');
 
 const outputPath = path.resolve(__dirname, './dev/public');
 
+// function getComponentPath(entry) {
+// 	return path.join(__dirname, 'test', 'dev', 'components', entry);
+// }
+
+// eslint-disable-next-line no-undef
 module.exports = {
 	devServer: {
 		compress: false,
@@ -50,8 +58,8 @@ module.exports = {
 			{
 				test: /\.(scss|css)$/,
 				use: [
-					{loader: 'style-loader'},
-					{loader: 'css-loader'},
+					{ loader: 'style-loader' },
+					{ loader: 'css-loader' },
 					{
 						loader: 'sass-loader',
 						options: {
@@ -66,7 +74,7 @@ module.exports = {
 										});
 									}
 									else {
-										done({file: url});
+										done({ file: url });
 									}
 								},
 							},
