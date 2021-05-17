89500cca5f6a5d6231dceaeaa460558dbd711e3a
garofalog <giulio.garofalo@liferay.com>
2021-05-17 22:50:36 +0200

    COMMERCE-5483 init portlet

--- modules/dxp/apps/commerce/commerce-shop-by-diagram/commerce-shop-by-diagram-web/src/main/resources/META-INF/resources/js/Diagram.js
+++ modules/dxp/apps/commerce/commerce-shop-by-diagram/commerce-shop-by-diagram-web/src/main/resources/META-INF/resources/js/Diagram.js
@@ -30,114 +30,119 @@ const Diagram = ({
 	spritemap,
 	zoomController,
 }) => {
-	const [addPinHandler, setAddPinHandler] = useState(false);
-	const [removePinHandler, setRemovePinHandler] = useState({
-		handler: false,
-		pin: null,
-	});
-	const [resetZoom, setResetZoom] = useState(false);
-	const [zoomInHandler, setZoomInHandler] = useState(false);
-	const [zoomOutHandler, setZoomOutHandler] = useState(false);
-	const [changedScale, setChangedScale] = useState(false);
-	const [scale, setScale] = useState(1);
-	const [selectedOption, setSelectedOption] = useState(1);
-	const [cPins, setCpins] = useState(pins);
-	const [showTooltip, setShowTooltip] = useState({
-		details: {
-			cx: 0,
-			cy: 0,
-			id: null,
-			label: '',
-			linked_to_sku: 'sku',
-			quantity: null,
-			sku: '',
-		},
-		tooltip: null,
-	});
-	const [addNewPinState, setAddNewPinState] = useState({
-		fill: newPinSettings.colorPicker.selectedColor,
-		radius: newPinSettings.defaultRadius,
-	});
+	// const [test] = useState('test')
 
-	useEffect(() => {
-		if (!showTooltip.tooltip && showTooltip.details.id) {
-			const myNewState = cPins.map((element) => {
-				if (element.id === showTooltip.details.id) {
-					return {
-						cx: cPins[element.id].cx,
-						cy: cPins[element.id].cy,
-						draggable: cPins[element.id].draggable,
-						fill: cPins[element.id].fill,
-						id: showTooltip.details.id,
-						label: showTooltip.details.label,
-						linked_to_sku: showTooltip.details.linked_to_sku,
-						quantity: showTooltip.details.quantity,
-						r: cPins[element.id].r,
-						sku: showTooltip.details.sku,
-					};
-				}
-				else {
-					return element;
-				}
-			});
-			setCpins(myNewState);
-		}
-	}, [showTooltip, setShowTooltip]);
+	return (<p>cicczo</p>)
 
-	return (
-		<div className="diagram mx-auto">
-			<DiagramHeader
-				addNewPinState={addNewPinState}
-				newPinSettings={newPinSettings}
-				setAddNewPinState={setAddNewPinState}
-				setAddPinHandler={setAddPinHandler}
-				setSelectedOption={setSelectedOption}
-			/>
 
-			<ImagePins
-				addNewPinState={addNewPinState}
-				addPinHandler={addPinHandler}
-				cPins={cPins}
-				changedScale={changedScale}
-				enablePanZoom={enablePanZoom}
-				enableResetZoom={enableResetZoom}
-				image={image}
-				imageSettings={imageSettings}
-				navigationController={navigationController}
-				removePinHandler={removePinHandler}
-				resetZoom={resetZoom}
-				scale={scale}
-				selectedOption={selectedOption}
-				setAddPinHandler={setAddPinHandler}
-				setChangedScale={setChangedScale}
-				setCpins={setCpins}
-				setRemovePinHandler={setRemovePinHandler}
-				setResetZoom={setResetZoom}
-				setScale={setScale}
-				setSelectedOption={setSelectedOption}
-				setShowTooltip={setShowTooltip}
-				setZoomInHandler={setZoomInHandler}
-				setZoomOutHandler={setZoomOutHandler}
-				showTooltip={showTooltip}
-				zoomController={zoomController}
-				zoomInHandler={zoomInHandler}
-				zoomOutHandler={zoomOutHandler}
-			/>
-
-			<DiagramFooter
-				changedScale={changedScale}
-				enableResetZoom={enableResetZoom}
-				selectedOption={selectedOption}
-				setAddPinHandler={setAddPinHandler}
-				setChangedScale={setChangedScale}
-				setResetZoom={setResetZoom}
-				setSelectedOption={setSelectedOption}
-				setZoomInHandler={setZoomInHandler}
-				setZoomOutHandler={setZoomOutHandler}
-				spritemap={spritemap}
-			/>
-		</div>
-	);
+	// const [addPinHandler, setAddPinHandler] = useState(false);
+	// const [removePinHandler, setRemovePinHandler] = useState({
+	// 	handler: false,
+	// 	pin: null,
+	// });
+	// const [resetZoom, setResetZoom] = useState(false);
+	// const [zoomInHandler, setZoomInHandler] = useState(false);
+	// const [zoomOutHandler, setZoomOutHandler] = useState(false);
+	// const [changedScale, setChangedScale] = useState(false);
+	// const [scale, setScale] = useState(1);
+	// const [selectedOption, setSelectedOption] = useState(1);
+	// const [cPins, setCpins] = useState(pins);
+	// const [showTooltip, setShowTooltip] = useState({
+	// 	details: {
+	// 		cx: 0,
+	// 		cy: 0,
+	// 		id: null,
+	// 		label: '',
+	// 		linked_to_sku: 'sku',
+	// 		quantity: null,
+	// 		sku: '',
+	// 	},
+	// 	tooltip: null,
+	// });
+	// const [addNewPinState, setAddNewPinState] = useState({
+	// 	fill: newPinSettings.colorPicker.selectedColor,
+	// 	radius: newPinSettings.defaultRadius,
+	// });
+	//
+	// useEffect(() => {
+	// 	if (!showTooltip.tooltip && showTooltip.details.id) {
+	// 		const myNewState = cPins.map((element) => {
+	// 			if (element.id === showTooltip.details.id) {
+	// 				return {
+	// 					cx: cPins[element.id].cx,
+	// 					cy: cPins[element.id].cy,
+	// 					draggable: cPins[element.id].draggable,
+	// 					fill: cPins[element.id].fill,
+	// 					id: showTooltip.details.id,
+	// 					label: showTooltip.details.label,
+	// 					linked_to_sku: showTooltip.details.linked_to_sku,
+	// 					quantity: showTooltip.details.quantity,
+	// 					r: cPins[element.id].r,
+	// 					sku: showTooltip.details.sku,
+	// 				};
+	// 			}
+	// 			else {
+	// 				return element;
+	// 			}
+	// 		});
+	// 		setCpins(myNewState);
+	// 	}
+	// }, [showTooltip, setShowTooltip]);
+	//
+	// return (
+	// 	<div className="diagram mx-auto">
+	// 		<DiagramHeader
+	// 			addNewPinState={addNewPinState}
+	// 			newPinSettings={newPinSettings}
+	// 			setAddNewPinState={setAddNewPinState}
+	// 			setAddPinHandler={setAddPinHandler}
+	// 			setSelectedOption={setSelectedOption}
+	// 		/>
+	//
+	// 		<ImagePins
+	// 			addNewPinState={addNewPinState}
+	// 			addPinHandler={addPinHandler}
+	// 			cPins={cPins}
+	// 			changedScale={changedScale}
+	// 			enablePanZoom={enablePanZoom}
+	// 			enableResetZoom={enableResetZoom}
+	// 			image={image}
+	// 			imageSettings={imageSettings}
+	// 			navigationController={navigationController}
+	// 			removePinHandler={removePinHandler}
+	// 			resetZoom={resetZoom}
+	// 			scale={scale}
+	// 			selectedOption={selectedOption}
+	// 			setAddPinHandler={setAddPinHandler}
+	// 			setChangedScale={setChangedScale}
+	// 			setCpins={setCpins}
+	// 			setRemovePinHandler={setRemovePinHandler}
+	// 			setResetZoom={setResetZoom}
+	// 			setScale={setScale}
+	// 			setSelectedOption={setSelectedOption}
+	// 			setShowTooltip={setShowTooltip}
+	// 			setZoomInHandler={setZoomInHandler}
+	// 			setZoomOutHandler={setZoomOutHandler}
+	// 			showTooltip={showTooltip}
+	// 			zoomController={zoomController}
+	// 			zoomInHandler={zoomInHandler}
+	// 			zoomOutHandler={zoomOutHandler}
+	// 		/>
+	//
+	// 		<DiagramFooter
+	// 			changedScale={changedScale}
+	// 			enableResetZoom={enableResetZoom}
+	// 			selectedOption={selectedOption}
+	// 			setAddPinHandler={setAddPinHandler}
+	// 			setChangedScale={setChangedScale}
+	// 			setResetZoom={setResetZoom}
+	// 			setSelectedOption={setSelectedOption}
+	// 			setZoomInHandler={setZoomInHandler}
+	// 			setZoomOutHandler={setZoomOutHandler}
+	// 			spritemap={spritemap}
+	// 		/>
+	// 	</div>
+	// );
 };
 
 Diagram.defaultProps = {
