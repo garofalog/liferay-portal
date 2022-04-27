6f60d8ae75a2d46dc0cae52e0b5b296cf778b8f6
Giulio Garofalo <giulio.garofalo@liferay.com>
2022-04-27 16:46:56 +0200

    COMMERCE-8888 modal taglib refactoring

---
+++ modules/apps/commerce/commerce-frontend-taglib/src/main/resources/META-INF/resources/js/modal/page.js
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import Modal from 'commerce-frontend-js/components/modal/entry';
+
+export default function ({
+	containerId,
+	id,
+	onClose,
+	portletId,
+	size,
+	spritemap,
+	title,
+	url,
+}) {
+	Modal(id, containerId, {
+		id,
+		onClose: onClose ? window.location.reload() : null,
+		portletId,
+		size,
+		spritemap,
+		title,
+		url,
+	});
+}
