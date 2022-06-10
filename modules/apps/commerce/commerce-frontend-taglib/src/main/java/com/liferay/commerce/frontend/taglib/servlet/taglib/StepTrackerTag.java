fb082e10611746dc1c50188450c5c3f3db28d18c
Giulio Garofalo <giulio.garofalo@liferay.com>
2022-06-11 00:35:40 +0200

    COMMERCE-8895 new reactRenderer steptracker

--- modules/apps/commerce/commerce-frontend-taglib/src/main/java/com/liferay/commerce/frontend/taglib/servlet/taglib/StepTrackerTag.java
+++ modules/apps/commerce/commerce-frontend-taglib/src/main/java/com/liferay/commerce/frontend/taglib/servlet/taglib/StepTrackerTag.java
@@ -16,21 +16,75 @@ package com.liferay.commerce.frontend.taglib.servlet.taglib;
 
 import com.liferay.commerce.frontend.model.StepModel;
 import com.liferay.commerce.frontend.taglib.internal.servlet.ServletContextUtil;
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.frontend.icons.FrontendIconsUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.theme.PortletDisplay;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.template.react.renderer.ComponentDescriptor;
+import com.liferay.portal.template.react.renderer.ReactRenderer;
 import com.liferay.taglib.util.IncludeTag;
 
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.PageContext;
 
 /**
  * @author Fabio Diego Mastrorilli
  */
 public class StepTrackerTag extends IncludeTag {
+
+	@Override
+	public int doStartTag() throws JspException {
+		try {
+			HttpServletRequest httpServletRequest = getRequest();
+
+			String randomKey =
+				PortalUtil.generateRandomKey(httpServletRequest, "taglib") +
+					StringPool.UNDERLINE;
+
+			String stepTrackerId = randomKey + "stepTrackerId";
+
+			ThemeDisplay themeDisplay =
+				(ThemeDisplay)httpServletRequest.getAttribute(
+					WebKeys.THEME_DISPLAY);
+
+			PortletDisplay portletDisplay = themeDisplay.getPortletDisplay();
+
+			Map<String, Object> data = HashMapBuilder.<String, Object>put(
+				"portletId", portletDisplay.getRootPortletId()
+			).put(
+				"spritemap", _spritemap
+			).put(
+				"steps", _steps
+			).build();
+
+			HttpServletResponse httpServletResponse =
+				(HttpServletResponse)pageContext.getResponse();
+
+			_reactRenderer.renderReact(
+				new ComponentDescriptor(
+					"commerce-frontend-js/components/step_tracker/StepTracker",
+					stepTrackerId),
+				data, httpServletRequest, httpServletResponse.getWriter());
+		}
+		catch (Exception exception) {
+			_log.error(exception);
+
+			return SKIP_BODY;
+		}
+
+		return SKIP_BODY;
+	}
 
 	public String getSpritemap() {
 		return _spritemap;
@@ -59,13 +113,9 @@ public class StepTrackerTag extends IncludeTag {
 	protected void cleanUp() {
 		super.cleanUp();
 
+		_reactRenderer = null;
 		_spritemap = null;
 		_steps = null;
-	}
-
-	@Override
-	protected String getPage() {
-		return _PAGE;
 	}
 
 	@Override
@@ -84,8 +134,9 @@ public class StepTrackerTag extends IncludeTag {
 			"liferay-commerce:step-tracker:steps", _steps);
 	}
 
-	private static final String _PAGE = "/step_tracker/page.jsp";
+	private static final Log _log = LogFactoryUtil.getLog(StepTrackerTag.class);
 
+	private ReactRenderer _reactRenderer;
 	private String _spritemap;
 	private List<StepModel> _steps;
 
